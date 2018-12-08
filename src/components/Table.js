abb4112844047b2047f87d1cad67c1cba6f968e6
Lucio Camargo <luciomcamargo@gmail.com>
2018-12-08 17:10:38 +0100

    cool

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,137 +1,346 @@
 import React, { Component } from "react";
 import "./Table.css";
-import axios from "axios";
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-  componentDidMount() {
-    axios.get(`http://localhost:3000/db.json`).then(res => {
-      const data = Object.values(res.data.data).map(({ title }) => ({
-        title
-      }));
-
-      this.setState({ data: data });
-    });
-  }
   render() {
-    const data = this.state.data.slice(0, 20);
-    const titles = data.map((titles, index) => (
-      <div className="grid-item" key={titles.title + index}>
-        <p key={titles.title}>{titles.title}</p>{" "}
-      </div>
-    ));
     return (
       <div className="grid-container">
         <div className="grid">
           <div className="grid-col--fixed-left">
-            <div className="grid-item--header" />
-            <div className="grid-item-col">
-              <p>Hello</p>
+            <div className="grid-item--header">
+              <p>Market</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>PL</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>PL</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>PL</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>TW</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>CZ</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>AT</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>AT</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>HU</p>
             </div>
             <div className="grid-item-col">
-              <p>Hello</p>
+              <p>HU</p>
+            </div>
+            <div className="grid-item-col">
+              <p>FR</p>
             </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>MIP Name</p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>TVN</p>
+            </div>
+            <div className="grid-item">
+              <p>TVP</p>
+            </div>
+            <div className="grid-item">
+              <p>TV Polsat</p>
+            </div>
+            <div className="grid-item">
+              <p>Zenith</p>
+            </div>
+            <div className="grid-item">
+              <p>Active Media Praha</p>
+            </div>
+            <div className="grid-item">
+              <p>Reichl und Partner GmbH</p>
+            </div>
+            <div className="grid-item">
+              <p>SevenVentures Austria GmbH</p>
+            </div>
+            <div className="grid-item">
+              <p>RTL Adconnect</p>
+            </div>
+            <div className="grid-item">
+              <p>YEHO Ltd</p>
+            </div>
+            <div className="grid-item">
+              <p>My Media</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Start date</p>
             </div>
 
-            {titles}
+            <div className="grid-item">
+              <p>12-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>12-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>12-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>15-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>1-Nov-2018</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>End date</p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>25-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>25-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>25-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>25-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>28-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>30-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>30-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>30-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>30-Nov-2018</p>
+            </div>
+            <div className="grid-item">
+              <p>30-Nov-2018</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Status</p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Draft</p>
+            </div>
+            <div className="grid-item">
+              <p>Draft</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
+            <div className="grid-item">
+              <p>Sent</p>
+            </div>
+            <div className="grid-item">
+              <p>Approved</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Brief budget</p>
             </div>
 
-            {titles}
+            <div className="grid-item">
+              <p>247.082,00zl</p>
+            </div>
+            <div className="grid-item">
+              <p>218.862,00zl</p>
+            </div>
+            <div className="grid-item">
+              <p>242.256,00zl</p>
+            </div>
+            <div className="grid-item">
+              <p>5.194.504,00$</p>
+            </div>
+            <div className="grid-item">
+              <p>469.105,00Kč</p>
+            </div>
+            <div className="grid-item">
+              <p>93.000,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>40.900,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>5.701.743,00Ft</p>
+            </div>
+            <div className="grid-item">
+              <p>2.391,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Brief budget in €</p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>57.769,93€</p>
+            </div>
+            <div className="grid-item">
+              <p>51.171,85€</p>
+            </div>
+            <div className="grid-item">
+              <p>56.641,57€</p>
+            </div>
+            <div className="grid-item">
+              <p>146.766,42€</p>
+            </div>
+            <div className="grid-item">
+              <p>18.224,75€</p>
+            </div>
+            <div className="grid-item">
+              <p>93.000,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>40.900,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>17.577,90€</p>
+            </div>
+            <div className="grid-item">
+              <p>2.391,00€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
           </div>
 
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Proposal in €</p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>57.769,93€</p>
+            </div>
+            <div className="grid-item">
+              <p>51.171,85€</p>
+            </div>
+            <div className="grid-item">
+              <p>56.641,57€</p>
+            </div>
+            <div className="grid-item">
+              <p>146.766,42€</p>
+            </div>
+            <div className="grid-item">
+              <p>18.231,12€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
+            <div className="grid-item">
+              <p>950.290,50€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
+            <div className="grid-item">
+              <p>0€</p>
+            </div>
           </div>
-
           <div className="grid-col">
             <div className="grid-item--header">
-              <p>HEAD</p>
+              <p>Proposal file</p>
             </div>
-            {titles}
-          </div>
-
-          <div className="grid-col">
-            <div className="grid-item--header">
-              <p>HEAD</p>
+            <div className="grid-item">
+              <p>
+                <i className="fas fa-file fa-2x" />
+              </p>
             </div>
-            {titles}
-          </div>
-
-          <div className="grid-col">
-            <div className="grid-item--header">
-              <p>HEAD</p>
+            <div className="grid-item">
+              <p>
+                <i className="fas fa-file fa-2x" />
+              </p>
             </div>
-            {titles}
+            <div className="grid-item">
+              <p>
+                <i className="fas fa-file fa-2x" />
+              </p>
+            </div>
+            <div className="grid-item">
+              <p>
+                <i className="fas fa-file fa-2x" />
+              </p>
+            </div>
+            <div className="grid-item">
+              <p>
+                <i className="fas fa-file fa-2x" />
+                <i className="fas fa-file fa-2x" />
+                <i className="fas fa-file fa-2x" />
+              </p>
+            </div>
+            <div className="grid-item">
+              <p />
+            </div>
+            <div className="grid-item">
+              <p />
+            </div>
+            <div className="grid-item">
+              <i className="fas fa-file fa-2x" />
+            </div>
+            <div className="grid-item">
+              <p />
+            </div>
+            <div className="grid-item">
+              <p />
+            </div>
           </div>
         </div>
       </div>
